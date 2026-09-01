776e4f260e23402423922b40e415bfdf3125b78f
Sathya Priya <sathyapriyababu91@gmail.com>
2026-09-01 21:49:57 +0530

    update backend url to render api file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://syncchat-rfzq.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
