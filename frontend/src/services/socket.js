2dd13cdfca1c4c4aa86da14beb5524a5ffcebd2c
Sathya Priya <sathyapriyababu91@gmail.com>
2026-09-01 22:02:22 +0530

    upadate socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://syncchat-rfzq.onrender.com", {
+  withCredentials: true,
+});
 
 export default socket;
